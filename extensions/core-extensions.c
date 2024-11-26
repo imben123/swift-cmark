5c3fe3a3922d1ec1770eb62db4b718882da12104
Ben Davis <imben123@gmail.com>
2024-11-26 15:24:54 +0000

    Added :: and == syntax as a highlight inline container

--- extensions/core-extensions.c
+++ extensions/core-extensions.c
@@ -3,6 +3,7 @@
 #include "mutex.h"
 #include "node.h"
 #include "strikethrough.h"
+#include "highlight.h"
 #include "table.h"
 #include "tagfilter.h"
 #include "tasklist.h"
@@ -13,6 +14,7 @@ static int core_extensions_registration(cmark_plugin *plugin) {
   cmark_plugin_register_syntax_extension(plugin, create_table_extension());
   cmark_plugin_register_syntax_extension(plugin,
                                          create_strikethrough_extension());
+  cmark_plugin_register_syntax_extension(plugin, create_highlight_extension());
   cmark_plugin_register_syntax_extension(plugin, create_autolink_extension());
   cmark_plugin_register_syntax_extension(plugin, create_tagfilter_extension());
   cmark_plugin_register_syntax_extension(plugin, create_tasklist_extension());
--- fuzz/fuzz_quadratic_brackets.c
+++ fuzz/fuzz_quadratic_brackets.c
@@ -11,6 +11,7 @@
 const char *extension_names[] = {
   "autolink",
   "strikethrough",
+  "highlight",
   "table",
   "tagfilter",
   NULL,
